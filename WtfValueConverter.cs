fcbc63645190f2e113f5013106153508bce819f9
andrewralon <acralon@gmail.com>
2015-08-07 11:50:21 -0500

    0.0.1.18 -Fixed EnumConverter error by using a custom boolToVisibility converter (and no longer using strings) -New WtfValueConverter.cs and WtfEnum

---
+++ WtfValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SlickProcess
+{
+    public class WtfValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((WtfEnum)value).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum WtfEnum
+    {
+        Hidden,
+        Visible,
+        Collapsed
+    }
+}
